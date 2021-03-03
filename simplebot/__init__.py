d8bf18467a1e9a6a8fb03199a92c3d703748abd0
Di SONG <songdi19@gmail.com>
2021-03-03 21:24:53 +1100

    update

--- simplebot/__init__.py
+++ simplebot/__init__.py
@@ -1,18 +1,17 @@
 import logging
 import sys
-from typing import Iterable, Optional, Dict
+from typing import Dict, Iterable, Optional
 
 from simplebot.api import SimpleRequest
 from simplebot.base import SimpleBotException, Update
 from simplebot.bot import SimpleBot
+from simplebot.handler import UpdateHandler
 from simplebot.router import SimpleRouter
 from simplebot.storage import SimpleStorage
-from simplebot.handler import UpdateHandler
 
 logger = logging.getLogger("simple-bot")
 formatter = logging.Formatter(
-    '%(levelname)s %(asctime)s (%(filename)s:%(lineno)d): "%(message)s"'
-)
+    '%(levelname)s %(asctime)s (%(filename)s:%(lineno)d): "%(message)s"')
 console_output_handler = logging.StreamHandler(sys.stderr)
 console_output_handler.setFormatter(formatter)
 logger.addHandler(console_output_handler)
@@ -32,14 +31,13 @@ class BotProxy:
 
     __slots__ = ("_bot_data", "_router_data", "_name")
 
-    def __init__(self, name: str = "default_proxy") -> None:
+    def __init__(self, name: Optional[str] = None) -> None:
         self._bot_data = {}
         self._router_data = {}
-        self._name = name
+        self._name = name or "default"
 
     @property
     def name(self):
-        """name."""
         return self._name
 
     def router(
@@ -47,7 +45,7 @@ class BotProxy:
         name: Optional[str] = None,
         handlers: Optional[Iterable[UpdateHandler]] = None,
     ) -> SimpleRouter:
-        name = name or "default_router"
+        name = name or "default"
         router = self._router_data.get(name, None)
         if router is None:
             self._router_data[name] = SimpleRouter(name, handlers)
@@ -55,34 +53,33 @@ class BotProxy:
             router.register_handlers(handlers)
         return self._router_data[name]
 
-    def create_bot(
-        self,
-        token: str,
-        router: Optional[SimpleRouter] = None,
-        handlers: Optional[Iterable[UpdateHandler]] = None,
-        storage: Optional[SimpleStorage] = None,
-        i18n_source: Optional[Dict] = None,
-        api_host: Optional[str] = None,
-        **urllib3_pool_kwargs
-    ):
-        if token in self._bot_data:
-            del self._bot_data[token]
+    def create_bot(self,
+                   token: str,
+                   router: Optional[SimpleRouter] = None,
+                   handlers: Optional[Iterable[UpdateHandler]] = None,
+                   storage: Optional[SimpleStorage] = None,
+                   i18n_source: Optional[Dict] = None,
+                   api_host: Optional[str] = None,
+                   **urllib3_pool_kwargs):
         router = router or self.router(handlers=handlers)
         self._bot_data[token] = SimpleBot(
             token,
             router,
             storage,
             i18n_source,
-            SimpleRequest(
-                api_host=api_host or "https://api.telegram.org", **urllib3_pool_kwargs
-            ),
+            SimpleRequest(api_host=api_host or "https://api.telegram.org",
+                          **urllib3_pool_kwargs),
         )
         return self._bot_data[token]
+
+    def bot(self, token: str) -> Optional[SimpleBot]:
+        return self._bot_data.get(token, None)
 
     async def dispatch(self, token: str, raw_update: Dict):
         simple_bot = self._bot_data.get(token, None)
         if simple_bot is None:
-            raise SimpleBotException("No bot found with token: '{0}'".format(token))
+            raise SimpleBotException(
+                "No bot found with token: '{0}'".format(token))
         await simple_bot.dispatch(Update(**raw_update))
 
 
