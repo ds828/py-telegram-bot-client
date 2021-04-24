88b0cefc38ee577bd7ed378189cba0f1c3cd356c
Di SONG <songdi19@gmail.com>
2021-04-24 16:25:12 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-from simpletelegrambot import __version__
+from telegrambotclient import __version__
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='simple-telegram-bot',
+    name='py-telegram-bot-client',
     version=__version__,
     description='A Telegram Bot API Client written by Python',
     long_description=long_description,
-    url='https://github.com/songdi/simple-telegram-bot',
+    url='https://github.com/songdi/py-telegram-bot-client',
     author='Di SONG',
     author_email='songdi19@gmail.com',
-    packages=['simpletelegrambot'],
+    packages=['telegrambotclient'],
     install_requires=['urllib3', 'redis', 'ujson'],
     python_requires=">=3.5",
 )
